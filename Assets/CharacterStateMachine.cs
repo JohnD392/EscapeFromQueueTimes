54923ecbe7c53eb53a9116088a7290d0cc964d52
John Dunn <JohnD392@yahoo.com>
2023-08-02 17:52:41 -0700

    Humble beginnings

---
+++ Assets/CharacterStateMachine.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterStateMachine : MonoBehaviour {
+    ICharacterState currentState;
+
+    public InputAction jump;
+
+    static ICharacterState idleState = new IdleState();
+    static ICharacterState movementState = new MovementState();
+
+    public void Initialize(ICharacterState startingState) {
+        currentState = startingState;
+        startingState.OnEnterState(this.gameObject);
+    }
+
+    public void ChangeState(ICharacterState newState) {
+        currentState.OnExitState(this.gameObject);
+        currentState = newState;
+        newState.OnEnterState(this.gameObject);
+    }
+
+    public void Update() {
+        currentState.Tick(this.gameObject);
+    }
+}
+
+public interface ICharacterState {
+    public void Tick(GameObject character);
+    public void OnEnterState(GameObject character);
+    public void OnExitState(GameObject character);
+}
+
+public class IdleState : ICharacterState {
+    public void OnEnterState(GameObject character) {
+        Debug.Log("I'm gonna chill now");
+    }
+
+    public void OnExitState(GameObject character) {
+        Debug.Log("I'm done chillin");
+    }
+
+    public void Tick(GameObject character) { }
+}
+
+public class MovementState : ICharacterState {
+    public void OnEnterState(GameObject character) {
+
+    }
+
+    public void OnExitState(GameObject character) {
+
+    }
+
+    public void Tick(GameObject character) {
+
+    }
+}
